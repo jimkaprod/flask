4cddc2a00985102d6de71956cd9c3f0766f0923d
jmcordier <jmcordier@laprovence.com>
2018-05-29 09:17:13 +0200

    profile avatar

--- app/models.py
+++ app/models.py
@@ -4,6 +4,8 @@ from app import db
 from flask_login import UserMixin
 from app import login
 from werkzeug.security import generate_password_hash, check_password_hash
+from hashlib import md5
+
 
 @login.user_loader
 def load_user(id):
@@ -16,6 +18,8 @@ class User(UserMixin,db.Model):
   email = db.Column(db.String(120), index=True, unique=True)
   password_hash = db.Column(db.String(128))
   posts = db.relationship('Post', backref='author', lazy='dynamic')
+  about_me = db.Column(db.String(140))
+  last_seen = db.Column(db.DateTime, default=datetime.utcnow)
 
   def __repr__(self):
     return '<User {}>'.format(self.username)
@@ -25,6 +29,11 @@ class User(UserMixin,db.Model):
 
   def check_password(self, password):
     return check_password_hash(self.password_hash, password)
+
+  def avatar(self, size):
+    digest = md5(self.email.lower().encode('utf-8')).hexdigest()
+    return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(
+      digest, size)
 
 
 class Post(db.Model):
