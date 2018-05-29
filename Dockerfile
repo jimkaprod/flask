1ba0f3e3f328ae6f8878a008a496c97c8d830f6b
jmcordier <jmcordier@laprovence.com>
2018-05-29 16:00:53 +0200

    pagination

--- Dockerfile
+++ Dockerfile
@@ -6,6 +6,7 @@ ADD . /home/app/
 WORKDIR /home/app/
 
 ENV FLASK_APP=microblog.py
+
 #ENV DATABASE_URL=db
 
 EXPOSE 5000
--- config.py
+++ config.py
@@ -21,4 +21,12 @@ class Config(object):
         port=DBPORT,
         db=DBNAME)
   SQLALCHEMY_TRACK_MODIFICATIONS = False
-  print(SQLALCHEMY_DATABASE_URI)
+
+  MAIL_SERVER = os.environ.get('MAIL_SERVER')
+  MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
+  MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+  MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+  MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+  ADMINS = ['jm@jimkaprod.com']
+
+  POSTS_PER_PAGE = 3
