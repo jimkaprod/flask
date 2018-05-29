ee6d6951a97da2aadc4b9c036715fffbc736d46c
jmcordier <jmcordier@laprovence.com>
2018-05-29 16:44:54 +0200

    boostrap

--- app/__init__.py
+++ app/__init__.py
@@ -8,6 +8,9 @@ import logging
 from logging.handlers import SMTPHandler
 from logging.handlers import RotatingFileHandler
 import os
+from flask_mail import Mail
+from flask_bootstrap import Bootstrap
+
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -15,6 +18,9 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
+mail = Mail(app)
+bootstrap = Bootstrap(app)
+
 
 from app import routes, models, errors
 
